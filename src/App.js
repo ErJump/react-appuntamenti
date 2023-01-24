08bb4adde8ef1ff16d025b4bfb6e993b6796844b
ErJump <fedelijunior@gmail.com>
2023-01-24 12:07:48 +0100

    feat: :sparkles: Adds list component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App bg-dark pt-5">
       <div className="container-lg">
+        <h1 className="text-primary mb-3 text-center">Appuntamenti:</h1>
         <List/>
       </div>
     </div>
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import {React, useState} from 'react'
+import data from '../data'
 
 export default function List() {
-  return (
-    <div>
-        <h1 className='text-white'>Lista</h1>
-    </div>
-  )
+
+    const [appointments, setAppointments] = useState(data);
+    
+    const removeAppointment = (id) =>{
+        setAppointments((oldAppointments) => {
+            return oldAppointments.filter(el => el.id !== id)
+        })
+    }
+
+    return (
+        <div className='row'>
+            {
+                appointments.map((appointment) => (
+                    <div key={appointment.id} className='col-12 ms_card py-3 mb-3 d-flex align-items-center'>
+                        <div className='me-3'>
+                            <img src={appointment.img} alt="img" />
+                        </div>
+                        <div>
+                            <h5 className='text-primary'>{appointment.name}</h5>
+                            <h6 className='text-white'>{appointment.status}</h6>
+                        </div>
+                        <button className='btn btn-danger ms-auto me-3' onClick={() => removeAppointment(appointment.id)}>X</button>
+                    </div>
+                ))
+            }
+            <div className="col-12 d-flex justify-content-between px-0">
+                <button className='btn btn-success'>Reset</button>
+                <button className='btn btn-danger'>Delete All</button>
+            </div>
+        </div>
+    )
 }
